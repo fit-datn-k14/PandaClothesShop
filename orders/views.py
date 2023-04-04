1b8de5ba8a5089e51ae7b7e93b89f604e7ebd078
HieuTran <hieu.tran3@sotatek.com>
2023-04-04 15:59:23 +0700

    view cart items in order products

--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from carts.models import CartItem
 from .forms import OrderForm
-from .models import Order, Payment
+from .models import Order, Payment, OrderProduct
 import datetime
 import json
 from django.http import HttpResponse
@@ -23,6 +23,20 @@ def payments(request):
     order.payment = payment
     order.is_ordered = True
     order.save()
+
+    # Move the cart items to Order Product table
+    cart_items = CartItem.objects.filter(user=request.user)
+
+    for item in cart_items:
+        order_product = OrderProduct()
+        order_product.order_id = order.id
+        order_product.payment = payment
+        order_product.user_id = request.user.id
+        order_product.user_id = request.user.id
+        order_product.quantity = item.quantity
+        order_product.product_price = item.product.price
+        order_product.ordered = True
+        order_product.save()
 
 
     return render(request, 'orders/payments.html')
